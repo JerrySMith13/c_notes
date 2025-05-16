a9063d7510e3a928ff67ba7eff4385783d299044
= <jeremy.ws.2008@gmail.com>
2025-05-16 09:53:40 -0500

    Committing everything before switching OS

---
+++ main.c
@@ -0,0 +1,25 @@
+
+/*
+ 
+Simple notes utility written in C
+Can add notes, group by subject, and view notes
+
+Notes organized by group, can print entire groups or specific entries
+
+-a {note contents} - adds note contents either to specified group or DEFAULT
+-g {group_id} - specifies group_id for adding, printing, etc. group_id will be set to default otherwise
+-p {contains: optional} - view notes, grepping for those that have the contains string
+-v, --verbose - specifies that printing of notes should be verbose
+--delete-note {note_id} - deletes the note from the specified group 
+--add-note - same as -a
+--group - same as -g
+--print - same as -p
+--create-group {group_id}- creates a new group with the specified id
+
+*/
+
+int main(int argc, char** argv){
+
+
+
+}
